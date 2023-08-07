9650e2f0cd73def69ab58f84b33178f87d27540f
Shadkoech <shadrackipruto@gmail.com>
2023-08-07 16:12:27 +0000

    Printing out an exact line

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 #A simple python program that aims to print given line
 
-output = '"Programming is like building a multilingual puzzle'
-print(output)
+print('"Programming is like building a multilingual puzzle')
