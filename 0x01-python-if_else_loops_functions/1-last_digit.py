7a6684bb1fe2792a2634e8e538b2e6e47345ed0d
Shadkoech <shadrackipruto@gmail.com>
2023-08-08 17:21:14 +0000

    Playing with numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,14 +5,10 @@ if number < 0:
     Lastdigit = -(number % 10)
 else:
     Lastdigit = (number % 10)
-
-# Assigning the appropriate message(Msg) in regard to last digit
-if Lastdigit > 5:
+if Lastdigit > 5:  # Assign message(Msg) in regard to last digit
     Msg = "and is greater than 5"
 elif Lastdigit == 0:
     Msg = "and is 0"
 else:
     Msg = "and is less than 6 and not 0"
-
-# Finally printing the desired output
 print(F"Last digit of {number} is {Lastdigit} {Msg}")
---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+for x in range(97, 123):
+    print(F'{chr(x)}', end="")  # chr() converts ASCII to corresponding char
