7da66df90ebeb257215e051f9c7182b50fb8e1d7
Shadkoech <shadrackipruto@gmail.com>
2023-08-11 12:38:41 +0000

    import statements

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+    """Import statement from cal module"""
     from calculator_1 import add, sub, mul, div
-    '''Import statement that imports names from calculator_1 module'''
+
     a = 10
     b = 5
+
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
