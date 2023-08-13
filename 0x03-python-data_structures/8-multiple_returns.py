7400fb576722994d408dc392c90e5e8673a6ca9c
Shadkoech <shadrackipruto@gmail.com>
2023-08-13 20:20:48 +0000

    append on more returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
+# tuple from a sentence
 def multiple_returns(sentence):
     """function returning a tuple with len of string and first char"""
-    my_tuple = ()
-    x = len(sentence)
-    y = sentence[0]
-    if x == 0:
-        my_tuple = 0, "None"
+
+    kl_tuple = ()
+    if len(sentence) == 0:
+        kl_tuple = (0, "None")
     else:
-        my_tuple = (x, y)
-        return my_tuple
+        kl_tuple = (len(sentence), sentence[0])
+        return kl_tuple
