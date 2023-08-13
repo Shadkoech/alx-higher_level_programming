f213ffa632fe71ab43401822b850559fb1914c0a
Shadkoech <shadrackipruto@gmail.com>
2023-08-13 12:59:29 +0000

    Returning element at an index

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# getting element at an index within a list range
+def element_at(my_list, idx):
+    '''Retrieving an element from a list'''
+    if idx < 1:
+        return None
+    elif idx >= len(my_list):
+        return None
+    else:
+        return my_list[idx]
