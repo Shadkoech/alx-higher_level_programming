d782aae8e8d8c9eadc1ca5e394d3002b3f901329
Shadkoech <shadrackipruto@gmail.com>
2023-08-16 23:50:51 +0000

    squaring all values of a matrix

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,18 @@
+#!/usr/bin/ls
+
+def weight_average(my_list=[]):
+    """function returning a weighted average of an integer
+    Weighted Average = (Sum of (Score * Weight)) / (Sum of Weights)
+    """
+
+    total_score = 0
+    total_weight = 0
+
+    if len(my_list) == 0:
+        return 0
+
+    for tple in my_list:
+        total_score += tple[0] * tple[1]
+        total_weight += tple[1]
+
+    return (total_score / total_weight)
