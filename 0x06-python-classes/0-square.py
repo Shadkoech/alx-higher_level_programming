c7b87f723610c07756a563db3591930a017d3e3a
Shadkoech <shadrackipruto@gmail.com>
2023-08-29 18:54:21 +0000

    Creating an empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-
+"""creating an empty class with just 3 lines of code """
 
 class Square:
-    """This is an empty class that defines a square"""
+    """This is an empty class that defines a square
+    a class is simply created by using the keyword class then
+    followed by a class name"""
     pass
