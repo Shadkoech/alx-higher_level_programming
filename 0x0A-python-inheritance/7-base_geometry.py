b63c3dab91b03b3d6d64032ebb93c87aaacf3080
Shadkoech <shadrackipruto@gmail.com>
2023-09-11 18:34:49 +0000

    import correction

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -25,12 +25,12 @@ class BaseGeometry:
         Method that validates value
         Args:
             name (str): name of value in question
-            value: actual value to be validated
+            value(in): actual value to be validated
         Raises:
             TypeError: if value is not int
             ValueError: if value is less or equal to 0
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,40 +1,9 @@
 #!/usr/bin/python3
 """
-Builds on the previously created empty class BaseGeometry by adding
-    A public instance method def area(self)
-    public instance method: def integer_validator(self, name, value):
-Returns:
-    class with an public instance method
+Creation of new class inheriting fr0m Basegeometry
 """
 
-
-class BaseGeometry:
-    """
-    Adding up public instance method area
-    """
-    def area(self):
-        """
-        Method that calculates the area of the Geometric class
-        Raises:
-            Exception: "area() is not implemented"
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Method that validates value
-        Args:
-            name (str): name of value in question
-            value (int): actual value to be validated
-        Raises:
-            TypeError: if value is not int
-            ValueError: if value is less or equal to 0
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """
