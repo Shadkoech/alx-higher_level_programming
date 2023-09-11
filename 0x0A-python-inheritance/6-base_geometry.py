13115c70bc81fa84b6c51b4a1351d754f46114c0
Shadkoech <shadrackipruto@gmail.com>
2023-09-11 17:10:43 +0000

    Addition of a public instance method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Builds on the previously created empty class BaseGeometry by adding
+a public instance method def area(self)
+Returns:
+    class with an public instance method
+"""
+
+
+class BaseGeometry:
+    """
+    Adding up public instance method area
+    """
+    def area(self):
+        """ Method that calculates the area of the Geometric class """
+        raise Exception("area() is not implemented")
