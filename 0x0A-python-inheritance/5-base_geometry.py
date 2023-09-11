a5e2493b3dd5278e9efee34a3d3a3d8f893c0258
Shadkoech <shadrackipruto@gmail.com>
2023-09-11 17:03:06 +0000

    Empty class creation

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+A module that writes out an empty class
+No attributes or methods initiated to it
+Returns:
+    Empty class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    empty class created
+    """
+    pass
