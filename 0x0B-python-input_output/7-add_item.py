0b9748f449b1ee0ae5d0d0116d74fa09c0276c39
Shadkoech <shadrackipruto@gmail.com>
2023-09-13 06:24:53 +0000

    Input/OutPut

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+import sys
+
 """
 A Python script that adds all available arguments to a python list
 then saves them all to a given file
 """
-import sys
 
 if __name__ == "__main__":
     """function that specifies what block of code ought
@@ -11,7 +12,8 @@ if __name__ == "__main__":
     """
 
     save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-    load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+    load_from_json_file = \
+        __import__("6-load_from_json_file").load_from_json_file
 
     try:
         addeditems = load_from_json_file("add_item.json")
