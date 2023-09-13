379bb3e11284ae7754c8acfc38750aed79300cf4
Shadkoech <shadrackipruto@gmail.com>
2023-09-13 06:34:48 +0000

    Input/OutPut

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import sys
 
 """
 A Python script that adds all available arguments to a python list
 then saves them all to a given file
 """
+import sys
+
 
 if __name__ == "__main__":
     """function that specifies what block of code ought
