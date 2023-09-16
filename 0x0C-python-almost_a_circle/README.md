c81a1d103b5502599c11395d1ad89b9637a97a8d
Shadkoech <shadrackipruto@gmail.com>
2023-09-16 07:34:44 +0000

    Base class

---
+++ 0x0C-python-almost_a_circle/README.md
@@ -0,0 +1,13 @@
+## 0x0C. Python - Almost a circle
+
+This project amalgamates a number of key Python concepts thus far. These concepts include:
+	* Import
+	* Exceptions
+	* Class
+	* Private attribute
+	* Getter/Setter
+	* Class method
+	* Static method
+	* Inheritance
+	* Unittest
+	* Read/Write file
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+""" A module of class rectangle that inherits from Class Base"""
+
+
+class Rectangle(Base):
+    """Defining a class Rectangle which is a subclass to Base"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """initialization of class Rectangle"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """getter to the triangle width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setting the triangle width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """privatizing and getting the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setting the height of the rectangle"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """getting the triangle's x-coordinate"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """setting the triangle's x-coordinate"""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """privatizing and getting y coordinate"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """setting the triangles's y coordinate"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """Method that calculates the area of a rectangle"""
+        return (self.width * self.height)
+
+    def display(self):
+        """Method that prints rectangle instance using character #"""
+        return(/n.join(['#' * self.width] * self.height))
