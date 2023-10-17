3becde5708996930536f59a7465640c5a42fa8e0
Shadkoech <shadrackipruto@gmail.com>
2023-10-17 20:14:59 +0000

    Getting average of scores as Average

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,6 @@
+-- SQL script that computes the average of all the records in the second_table
+-- The resultant column name should be average
+
+
+SELECT AVG(score) AS average
+FROM second_table
--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 -- SQL script that deletes the database hbtn_0c_0 in your MySQL server.
 -- Incase database hbtn_0c_0 doesn’t exist, your script should not fail
-DROP DATABASE IF EXISTS `hbtn_0c_0`;
+DROP DATABASE IF EXISTS hbtn_0c_0;
