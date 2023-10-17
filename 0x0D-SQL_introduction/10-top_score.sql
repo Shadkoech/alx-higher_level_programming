874ed8c0eac9af758ba530d634329467a781b980
Shadkoech <shadrackipruto@gmail.com>
2023-10-17 19:03:15 +0000

    List by best

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,7 @@
+-- Script that lists all records of the second_table of the database 
+-- The output should containt score and the name 
+-- Records ordered by score(descending)
+
+SELECT score, name
+FROM second_table
+ORDER BY score DESC
