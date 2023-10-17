f5e44424fd72bff59bf23a58476709e44e3be6f1
Shadkoech <shadrackipruto@gmail.com>
2023-10-17 22:37:01 +0000

    typo update

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,7 +2,7 @@
 -- Second table description: id(INT), name(VARCHAR(256)), Score(INT)
 -- The script should also create records into the database
 
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(257), score INT);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
 INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 13);
 INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
