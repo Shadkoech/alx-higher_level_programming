5d228e3b7694a3da1c7475dfb5d8272c35730ca3
Shadkoech <shadrackipruto@gmail.com>
2023-10-18 14:44:36 +0000

    Creating a table named force_name

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,4 @@
+-- SQL script creating a table force_name on MySQL server
+-- The table contains id INT and name VARCHAR(256)
+
+CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL);
