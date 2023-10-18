f55d4141c2cb01b6e5aa93a2d3204861c755c8cb
Shadkoech <shadrackipruto@gmail.com>
2023-10-18 14:13:38 +0000

    Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,4 +3,8 @@
 -- user_0d_1 passwords set to user_0d_1_pwd
 -- Script shouldn't break in the case 
 
-
+CREATE USER IF NOT EXISTS user_0d_1@localhost
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL
+ON *.*
+TO user_0d_1@localhost;
