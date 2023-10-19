b478565ea04d9e7e1fc9c962ac45c59683c9a3fa
Shadkoech <shadrackipruto@gmail.com>
2023-10-19 08:45:56 +0000

    Temperature profiles

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,9 @@
+-- Script displaying the top 3 cities of this table dump
+-- Ordered by temperature
+
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+WHERE month = 7 OR month = 8
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
