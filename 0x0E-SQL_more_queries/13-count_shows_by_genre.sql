e13c2d511d0906082817e97af504a62e57feb100
Shadkoech <shadrackipruto@gmail.com>
2023-10-19 09:55:43 +0000

    Temperature 1 and 2

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -6,9 +6,10 @@
 -- Output in descending order by the number of shows linked
 
 
-SELECT g.name AS genre, COUNT(*) AS number_of_shows
-FROM tv_genres AS g
-INNER JOIN tv_shows_genre AS t
-ON g.id = t.genre_id
+SELECT g.name AS genre,
+	COUNT(*) AS number_of_shows
+  FROM tv_genres AS g
+	INNER JOIN tv_shows_genre AS t
+	ON g.id = t.genre_id
 GROUP BY g.name
 ORDER BY number_of_shows DESC
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -3,7 +3,7 @@
 -- Output is sorted in ascending order by cities.id
 
 SELECT c.id, c.name, s.name
-FROM cities as c
+FROM cities AS c
 INNER JOIN states AS s
 ON c.state_id = s.id
 ORDER BY c.id ASC;
