6326fd96aa4ed5dc32774845957c609c04924742
Shadkoech <shadrackipruto@gmail.com>
2023-10-19 01:06:28 +0000

    Joining three tables tv_genres, tv_show_genres and tv_shows

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,14 @@
+-- Script using hbtn_0d_tvshows database to list all the genres of show Dexter
+-- Shows table contain one record where title = Dexter
+-- Each record must display tv_genres.name
+-- Output ascending by genre name
+
+SELECT g.name
+ FROM tv_genres as g
+	INNER JOIN tv_show_genres AS s
+	ON g.id = s.genre_id
+
+	INNER JOIN tv_shows AS t
+	ON t.id = s.show_id
+	WHERE t.title = "Dexter"
+ ORDER BY g.name;
