c2f76d115c3944de3a0ce24776e8dcfff9f7c772
Shadkoech <shadrackipruto@gmail.com>
2023-10-19 10:39:31 +0000

    Best genre update

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -3,7 +3,7 @@
 -- Every record should display: tv_shows.title
 -- Output sorted in ascending order by the show title
 
-SELECT name, SUM(tv_show_ratings.rate) 'rating'
+SELECT name, SUM(rate) AS 'rating'
 FROM tv_genres AS g
 INNER JOIN tv_show_genres AS s ON g.id = s.genre_id
 INNER JOIN tv_show_ratings AS r ON s.show_id = r.show_id
