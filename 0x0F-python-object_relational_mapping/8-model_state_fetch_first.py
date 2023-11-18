d16547d27338df3da5555cbe3be6770d1563d1e4
Shadkoech <shadrackipruto@gmail.com>
2023-11-18 09:56:38 +0000

    Add a script that prints the first State object from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""A Script that lists all the state objects from
-the database hbtn_0e_6_usa using the module
-SQLAlchemy"""
+"""A python script that prints the first State object from
+the database hbtn_0e_6_usa"""
 
 
 if __name__ == '__main__':
@@ -15,13 +14,13 @@ if __name__ == '__main__':
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
 
-    # Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
-
     session = Session()
 
-    for state in session.query(State).order_by(State.id).all():
-        print("{}: {}".format(state.id, state.name))
+    state = session.query(State).order_by(State.id).first()
+    if state is None:
+        print('Nothing')
+    else:
+        print(F"{state.id}: {state.name}")
 
     session.close()
