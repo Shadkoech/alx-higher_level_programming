8754e43a2bdf34c0e0ad7e5d4ac9b18d6a520782
Shadkoech <shadrackipruto@gmail.com>
2023-11-18 03:02:31 +0000

    Add script that lists all states with a name starting with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Python module that lists all states with a name
+starting with N (caps) from the database hbtn_0e_0_usa
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+            )
+
+    cur = db.cursor()
+
+    cur.execute("""
+        SELECT *
+        FROM states
+        WHERE name LIKE BINARY '%N'
+        ORDER BY states.id ASC;
+    """)
+
+    selected_rows = cur.fetchall()
+
+    for row in selected_rows:
+        print(row)
+
+    # close the cursor and database connection
+    cur.close()
+    db.close()
