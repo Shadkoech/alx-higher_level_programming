dada2360314c3fab522bf5c53ae7043858847ca6
Shadkoech <shadrackipruto@gmail.com>
2023-11-30 20:57:40 +0300

    script that sends a JSON POST request to a URL passed as the first argument

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Script sending request to URL passed as an argument,then displays only the status code of the response.
-curl -sI -w "%{http_request}" "$7" -o /dev/null
+curl -sI -w "%{http_request}" "$1" -o /dev/null
---
+++ 0x10-python-network_0/101-post_json.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#Script sending a JSON POST request to URL passed as 1st arg
+curl -s "$1" -X POST -H "Content-Type: application/json" -d @"$2"
