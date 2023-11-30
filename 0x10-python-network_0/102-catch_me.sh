7c28d84827d820ceb7596fad38150b7b404a59fa
Shadkoech <shadrackipruto@gmail.com>
2023-11-30 21:23:08 +0300

    script that makes a request to 0.0.0.0:5000/catch_me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Script making a request to 0.0.0.0:5000/catch_me making the server respond with You got me!, in the body of the response
-curl -X POST http://0.0.0.0:5000/catch_me -d '{"message":"You got me!"}'
+curl -X POST http://0.0.0.0:5000/catch_me -d '{"message":"You got me!"}' -H "Content-Type: application/json"
