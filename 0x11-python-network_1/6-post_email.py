9d87c7d3cb95e1766e30cbb771d7959400e0e59a
Shadkoech <shadrackipruto@gmail.com>
2023-12-01 18:23:32 +0300

    script that takes in a URL and an email address, sends a POST request

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Python module that takes in URL and an email then
+sends a POST request to the passed URL with email as
+parameter"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+
+    req = requests.post(url, value)
