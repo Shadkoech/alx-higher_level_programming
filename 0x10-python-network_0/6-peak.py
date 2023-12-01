3c06e8038d61f8a847346b4d8437562155bc912b
Shadkoech <shadrackipruto@gmail.com>
2023-12-01 05:46:20 +0300

    finding the peak of an unsorted list

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Python module that finds a peak value from an unsorted list
+The module handles a case where multiple elementd can qualify
+as peaks
+"""
+
+
+def find_peak(list_of_integers):
+    """Function that returns the max element from unsorted list
+    Argument:
+        list_of_integer - which is the unsorted list
+    """
+
+    if not list_of_integers:
+        return None
+
+    u_list = list_of_integers
+    length = len(u_list)
+    midpoint = int(length / 2)
+
+    # middle element is the only one in the list
+    if midpoint - 1 < 0 and midpoint + 1 >= length:
+        return u_list[midpoint]
+
+    elif midpoint - 1 < 0:
+        if u_list[midpoint] > u_list[midpoint +1]:
+            return u_list[midpoint]
+        else:
+            return u_list[midpoint +1]
+
+    elif midpoint + 1 >= length:
+        if u_list[midpoint] > u_list[midpoint - 1]:
+            return u_list[midpoint]
+        else:
+            return u_list[midpoint -1]
+
+    if u_list[midpoint - 1] < u_list[midpoint] > u_list[midpoint + 1]:
+        return u_list[midpoint]
+
+    if u_list[midpoint + 1] > u_list[midpoint -1]:
+        return find_peak(u_list[midpoint:])
+    return find_peak(u_list[:midpoint])
