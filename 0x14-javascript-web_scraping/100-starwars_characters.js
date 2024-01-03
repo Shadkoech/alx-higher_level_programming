fb2b3a70abda1ef071bf152fd9761d374353be1a
Shadkoech <shadrackipruto@gmail.com>
2024-01-03 23:49:47 +0300

    “Return of the Jedi”

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -5,22 +5,15 @@ const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
 request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-
-  const data = JSON.parse(body);
-  const actors = data.actors;
-
-  for (const actor of actors) {
-    request(actor, (error, response, body) => {
-      if (error) {
-        console.log(error);
-        return;
-      }
-      const actorData = JSON.parse(body);
-      console.log(actorData.name);
-    });
+  if (error == null) {
+    const resp = JSON.parse(body);
+    const characters = resp.characters;
+    for (let i = 0; i < characters.length; i++) {
+      request(characters[i], function (err, response, body) {
+        if (err == null) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
   }
 });
